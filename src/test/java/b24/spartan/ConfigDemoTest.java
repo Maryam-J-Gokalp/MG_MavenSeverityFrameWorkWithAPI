739bb0d312ca72694f5383fbf2a322eac2e8b849
Maryam-J-Gokalp <gokalp.maryam@gmail.com>
2022-08-07 14:14:36 +0100

    final commit

---
+++ src/test/java/b24/spartan/ConfigDemoTest.java
@@ -0,0 +1,18 @@
+package b24.spartan;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import utilities.ConfigReader;
+@Disabled
+public class ConfigDemoTest {
+
+
+
+    @Test
+    public void test1(){
+        System.out.println(ConfigReader.getProperty("serenity.project.name"));
+        System.out.println(ConfigReader.getProperty("spartan.editor.username"));
+
+
+    }
+}
